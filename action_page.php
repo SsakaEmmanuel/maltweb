3d2a900e6e8cb6555be73b976ae62945a1964cd5
SsakaEmmanuel <152312617+SsakaEmmanuel@users.noreply.github.com>
2023-11-28 20:03:36 +0300

    Add initial website files

---
+++ action_page.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>myphpfile</title>
+</head>
+<body>
+<center><h1> SUBMITED FOAM </h1></center>
+
+Welcome:
+<? php echo $_POST["first name"]; ?> <br><br>
+
+
+Your class is:
+<? php echo $_POST["class"]; ?>     <br><br>
+
+Your age is:
+<? php echo $_POST["age"]; ?>       <br><br>
+
+Your registration number is:
+<? php echo $_POST["reg no"]; ?>    <br><br>
+
+Your contact is:
+<? php echo $_POST["contact"]; ?>   <br><br>
+
+your gender is:
+<? php echo $_POST["gender"]; ?>    <br><br>
+
+Term:
+<? php echo $_POST["term"]; ?>      <br><br>
+
+Year:
+<? php echo $_POST["year"]; ?>      <br><br>
+
+
+</body>
+</html>
