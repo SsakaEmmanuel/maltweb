b91a654a73a3ff36dd637b4d917bff2a689b2008
Xhakapro3 <152312617+SsakaEmmanuel@users.noreply.github.com>
2024-02-15 13:46:50 +0300

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -7,11 +7,25 @@ $dbname = "database";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+
+// Create database
+$sql = "CREATE DATABASE myDB";
+if ($conn->query($sql) === TRUE) {
+  echo "Database created successfully";
+} else {
+  echo "Error creating database: " . $conn->error;
+}
+
+
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 echo "Registered successfully"
 
+} 
+
 $_POST["first name","class", "age", "reg no", "contact", "gender", "term", "year"];
- ?>
+
+$conn->close();
+?>
