9489dc7c9523c27766c5291dd6c842983e2d4065
Xhakapro3 <152312617+SsakaEmmanuel@users.noreply.github.com>
2024-02-15 11:16:05 +0300

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -1,25 +1,19 @@
 
 
-<? php echo $_POST["first name"]; ?>
+<? php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "mydatabase";
 
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-Your class is:
-<? php echo $_POST["class"]; ?>
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully"
 
-Your age is:
-<? php echo $_POST["age"]; ?>
-
-Your registration number is:
-<? php echo $_POST["reg no"]; ?>
-
-Your contact is:
-<? php echo $_POST["contact"]; ?>
-
-your gender is:
-<? php echo $_POST["gender"]; ?>
-
-Term:
-<? php echo $_POST["term"]; ?>
-
-Year:
-<? php echo $_POST["year"]; ?>
+$_POST["first name","class", "age", "reg no", "contact", "gender", "term", "year"];
+ ?>
